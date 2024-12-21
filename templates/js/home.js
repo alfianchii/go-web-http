32b054fda34ad846e094e4b73790793c136b2b40
alfianchii <pianaee379@gmail.com>
2024-12-21 21:04:42 +0700

    feat: username in chat

--- templates/js/home.js
+++ templates/js/home.js
@@ -4,7 +4,14 @@ import { validateJWT } from "./utils/token.js";
 
 const elLogout = document.getElementById("logout");
 let clientId;
-const generateDate = (str) => str ? new Date(str).toISOString().replace('T', ' ').split('.')[0] : new Date().toISOString().replace('T', ' ').split('.')[0];
+const generateDate = (str) => {
+  const date = new Date(str);
+  const options = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+  console.log(formattedDate);
+  const [day, month, year, time] = formattedDate.split(/,?\s+/);
+  return `${year}, ${month} ${day} at ${time}`;
+};
 const sendData = (socket, data) => socket.send(JSON.stringify(data));
 const initDOMElements = () => ({
   inputEl: document.getElementById("chat"),
@@ -41,33 +48,33 @@ const setupSocket = ({ inputEl, buttonEl, notifEl, messageListEl }) => {
 
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
-    displayChats(data, messageListEl)
-    if (data?.typers) updateNotification(data.typers, notifEl);
-    if (data?.send) displayMessage(data.text, messageListEl, data.createdAt);
+    displayChats(messageListEl, data)
+    if (data?.typers) updateNotification(notifEl, data.typers);
+    if (data?.send) displayMessage(messageListEl, data);
   };
 }
 
-const displayChats = (data, msgEl) => {
+const displayChats = (msgEl, data) => {
   if (Array.isArray(data)) 
-    for (const message of data) createTextElement("chats", msgEl, `${generateDate(message.createdAt)}: ${message.text}`);
+    for (const chat of data) createTextElement("chats", msgEl, chat);
 }
 
-const updateNotification = (typers, notifEl) => {
+const updateNotification = (notifEl, typers) => {
   if (typers.length === 0) notifEl.innerText = "Online";
   if (typers.length === 1) notifEl.innerText = `${typers[0]} is typing...`;
   if (typers.length > 1) notifEl.innerText = `${typers.join(", ")} are typing...`;
 }
 
-const displayMessage = (text, msgEl, date) => createTextElement("message", msgEl, `${generateDate(date)}: ${text}`);
+const displayMessage = (msgEl, message) => createTextElement("message", msgEl, message);
 
 const resetInput = (input) => {
   input.focus();
   input.value = "";
 }
 
-const createTextElement = (type, msgEl, text) => {
+const createTextElement = (type, msgEl, data) => {
   const newEl = document.createElement("p");
-  newEl.innerText = text;
+  newEl.innerHTML = createText(data);
 
   if (type === "chats") msgEl.appendChild(newEl);
   if (type === "message") {
@@ -75,6 +82,8 @@ const createTextElement = (type, msgEl, text) => {
     else msgEl.appendChild(newEl);
   }
 }
+
+const createText = (data) => `${generateDate(data.createdAt)} - <span style='background-color: yellow;'>${data.clientId}</span>: ${data.text}`
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
