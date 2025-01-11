eb50a27ee06d1461e4486b7f6e4f043965ebefa4
alfianchii <pianaee379@gmail.com>
2025-01-11 19:05:02 +0700

    feat(docker): mongo v8.0.4

--- docker/entrypoints/go.sh
+++ docker/entrypoints/go.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e # Exit immediately if a command exits with a non-zero status.
 
-# Run go mod tidy, go mod download, and go mod verify
 go mod tidy && go mod download && go mod verify
 air -c air.toml
