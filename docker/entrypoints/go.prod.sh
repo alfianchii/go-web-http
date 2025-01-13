d8db992a6c5a78cf9ea6c978e7ea557a15e3d6bc
alfianchii <pianaee379@gmail.com>
2025-01-13 16:51:00 +0700

    feat(docker): development and production environment

--- docker/entrypoints/go.prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e # Exit immediately if a command exits with a non-zero status.
-
-go mod tidy && go mod download && go mod verify
-go build -buildvcs=false -o main .
-./main
--- docker/entrypoints/go.sh
+++ docker/entrypoints/go.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 set -e # Exit immediately if a command exits with a non-zero status.
 
-go mod tidy && go mod download && go mod verify
-air -c air.toml
+APP_ENV=${APP_ENV:-production}
+
+echo "Running in ${APP_ENV} environment"
+
+if [ "$APP_ENV" = "development" ]; then
+  echo "Setting up for development..."
+  apk add --no-cache git && go install github.com/air-verse/air@latest
+  go mod tidy && go mod download && go mod verify
+  air -c air.toml
+elif [ "$APP_ENV" = "production" ]; then
+  echo "Setting up for production..."
+  apk add --no-cache git gettext
+  go mod tidy && go mod download && go mod verify
+  go build -buildvcs=false -o main .
+  ./main
+else
+  echo "Unknown APP_ENV value: ${APP_ENV}"
+  exit 1
+fi
