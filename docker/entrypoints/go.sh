f27dc304a8249ea8a8c0a5c56771dc59381c8b35
alfianchii <pianaee379@gmail.com>
2025-01-14 14:24:38 +0700

    chore: go mod tidy, download, and verify

--- docker/entrypoints/go.sh
+++ docker/entrypoints/go.sh
@@ -8,12 +8,12 @@ echo "Running in ${APP_ENV} environment"
 if [ "$APP_ENV" = "development" ]; then
   echo "Setting up for development..."
   apk add --no-cache git && go install github.com/air-verse/air@latest
-  go mod tidy && go mod verify
+  go mod tidy && go mod download && go mod verify
   air -c air.toml
 elif [ "$APP_ENV" = "production" ]; then
   echo "Setting up for production..."
   apk add --no-cache git gettext
-  go mod tidy && go mod verify
+  go mod tidy && go mod download && go mod verify
   go build -buildvcs=false -o main .
   ./main
 else
