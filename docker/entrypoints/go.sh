eb72b5090e8820eb60fae2f0c862ad3b8d4a9409
alfianchii <pianaee379@gmail.com>
2025-01-20 18:48:34 +0700

    refactor(docker): go entrypoint check `go.sum` and `main` file firwst

--- docker/entrypoints/go.sh
+++ docker/entrypoints/go.sh
@@ -7,16 +7,43 @@ echo "Running in ${APP_ENV} environment"
 
 if [ "$APP_ENV" = "development" ]; then
   echo "Setting up for development..."
-  apk add --no-cache git && go install github.com/air-verse/air@latest
-  go mod tidy && go mod download && go mod verify
+
+  # Air
+  if ! command -v air >/dev/null 2>&1; then
+    echo "Air not found, installing..."
+    apk add --no-cache git && go install github.com/air-verse/air@latest
+  else
+    echo "Air already installed."
+  fi
+
+  # go.sum file
+  if [ -f "go.sum" ]; then
+    echo "'go.sum' exists, skipping go mod tidy and go mod download."
+  else
+    echo "'go.sum' not found, running go mod tidy and go mod download."
+    go mod tidy && go mod download && go mod verify
+  fi
+  
   air -c air.toml
 elif [ "$APP_ENV" = "production" ]; then
   echo "Setting up for production..."
   apk add --no-cache git gettext
-  go mod tidy && go mod download && go mod verify
-  go build -buildvcs=false -o main .
+  
+  # go.sum file
+  if [ -f "go.sum" ]; then
+    echo "'go.sum' exists, skipping go mod tidy and go mod download."
+  else
+    echo "'go.sum' not found, running go mod tidy and go mod download."
+    go mod tidy && go mod download && go mod verify
+  fi
+
+  # main file
+  if [ -f "main" ]; then
+    echo "main exists, skipping go build."
+  else
+    echo "'main' not found, running go build."
+    go build -buildvcs=false -o main .
+  fi
+
   ./main
-else
-  echo "Unknown APP_ENV value: ${APP_ENV}"
-  exit 1
 fi
